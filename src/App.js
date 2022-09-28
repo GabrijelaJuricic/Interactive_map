0d79a9fff32ac7be9cc7e2b83f06c6e26c9bf35d
GabrijelaJuricic <gabjuricic@gmail.com>
2022-09-28 15:13:34 +0200

    Add page switch buttons and logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import MyMap from "./Components/MyMap";
+import Table from "./Components/Table";
+import "./App.css";
 
 const App = () => {
-  return <MyMap />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MyMap />}></Route>
+        <Route path="/table" element={<Table />}></Route>
+      </Routes>
+      <div className="btn-group" role="group" aria-label="Basic example">
+        <button type="button" className="btn btn-secondary">
+          <Link to="/" className="link">
+            Karta
+          </Link>
+        </button>
+        <button type="button" className="btn btn-secondary">
+          <Link to="/table" className="link">
+            Tablica
+          </Link>
+        </button>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
