3aa1e0e1003231750f41310e80766e17390c26c3
GabrijelaJuricic <gabjuricic@gmail.com>
2022-10-06 16:06:25 +0200

    Add toggle (layers)

--- src/Components/MyMap.js
+++ src/Components/MyMap.js
@@ -38,6 +38,10 @@ const MyMap = () => {
         [41.9, 20],
       ]}
     >
+      <label className="switch">
+        <input type="checkbox" />
+        <span className="slider round"></span>
+      </label>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
